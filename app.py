b8119f92db75c4f5de9c9157ae2c9bbc464df53a
Pavo <pzornija1@etf.unsa.ba>
2022-02-23 02:39:40 +0100

    Update

--- app.py
+++ app.py
@@ -242,24 +242,6 @@ def process_image():
     #     'label': label
     # }
 
-# load and prepare an image
-
-
-def load_image_pixels(filename, shape):
-    # load the image to get its shape
-    image = load_img(filename)
-    width, height = image.size
-    # load the image with the required size
-    image = load_img(filename, target_size=shape)
-    # convert to numpy array
-    image = img_to_array(image)
-    # scale pixel values to [0, 1]
-    image = image.astype('float32')
-    image /= 255.0
-    # add a dimension so that we have one sample
-    image = np.expand_dims(image, 0)
-    return image, width, height
-
 
 @app.route('/')
 def index_page():
